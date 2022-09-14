b8e199250d16c7fb628e046274479f49a18841a5
assacm <alma_julissa@live.com.mx>
2022-09-13 15:21:52 -1100

    Servicio users en pendientes

--- src/app/capturar/capturar.page.ts
+++ src/app/capturar/capturar.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormArray, Validators, FormBuilder, FormControl, RequiredValidator } from '@angular/forms';
 import { of } from 'rxjs';
 import { AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { LoginServiceService } from '../servicios/login-service.service';
 
 @Component({
   selector: 'app-capturar',
@@ -11,6 +13,13 @@ import { AlertController } from '@ionic/angular';
 })
 export class CapturarPage implements OnInit {
   fgCaptura: FormGroup;
+  token:any = localStorage.getItem('token');
+  id: any;
+  nombre: string;
+  medidor: string;
+  direccion: string;
+  mes: string;
+  usuarios: any;
   
   public form = [
     { val: 'Ninguna', isChecked: true },
@@ -25,7 +34,13 @@ export class CapturarPage implements OnInit {
     spaceBetween:10,
    
   }
-  constructor(private alertController: AlertController, public fb: FormBuilder, private router: Router) { 
+  constructor(
+    private alertController: AlertController, 
+    public fb: FormBuilder, 
+    private router: Router, 
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute,
+    private loginService:LoginServiceService) { 
     this.fgCaptura = this.fb.group({
       "lectura": new FormControl("",Validators.compose([Validators.required,Validators.minLength(10)]) ),
       "confirmar": new FormControl("",Validators.compose([Validators.required,Validators.minLength(10)]) )
@@ -34,6 +49,18 @@ export class CapturarPage implements OnInit {
   }
 
   ngOnInit() {
+    this.id = this.activatedRoute.snapshot.paramMap.get("id");
+    this.loginService.getUsers('smartmetricapi/readingss', this.token).subscribe(res=>{
+      console.log("res", res)
+      console.log('wenas wenas')
+      this.usuarios= res;
+
+      //this.nombre=  this.usuarios[this.id].name;
+     // this.medidor= this.usuarios[this.id]
+      //this.direccion=
+      //this.mes=
+      //this.usuarios=
+    });
   }
   
   validacion(){
