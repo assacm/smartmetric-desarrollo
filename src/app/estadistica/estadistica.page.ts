b0a2383bf8f5be37f6a51b612677d58142fc0b22
assacm <alma_julissa@live.com.mx>
2022-11-19 17:26:35 -1100

    Diseño estadistica

--- src/app/estadistica/estadistica.page.ts
+++ src/app/estadistica/estadistica.page.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estadistica.page.scss'],
 })
 export class EstadisticaPage implements OnInit {
+  
+  route = 42;
+  pending = 25;
+  complete = 2;
+  anomalies = 16;
+  time = 50;
+  currentTime = 20;
 
   constructor() { }
 
