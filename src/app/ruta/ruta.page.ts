61084d77a07447f1a93808f4144dd76e7f512649
Jesus Tarazon <tarazonjesus11@gmail.com>
2022-12-11 15:12:14 -0700

    Rutas y lotes en orden

--- src/app/ruta/ruta.page.ts
+++ src/app/ruta/ruta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { validValue } from '../functions';
 
 @Component({
   selector: 'app-ruta',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaPage implements OnInit {
 
+  storage = {
+    lotes :JSON.parse(localStorage.getItem('route')),
+    employee :JSON.parse(localStorage.getItem('employee'))
+  }
+
+  lotes;
+  employee;
+
   constructor() { }
 
   ngOnInit() {
+    if(validValue(this.storage.lotes) != false){
+      this.lotes= this.storage.lotes;
+      }
+      if(validValue(this.storage.employee) != false){
+        this.employee= this.storage.employee;
+        }
   }
+
+
+  
 
 }
